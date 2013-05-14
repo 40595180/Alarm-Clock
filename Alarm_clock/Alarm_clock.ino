eb183eb289c41b753111fd42dc2bcbcf58829bcf
Louie Thiros <lsthiros@gmail.com>
2013-05-13 23:17:11 -0700

    Display code now an ISR

--- Alarm_clock/Alarm_clock.ino
+++ Alarm_clock/Alarm_clock.ino
@@ -1,9 +1,14 @@
 #include <Wire.h>
 
+//pin definitions
 int latchPin=8;
 int clockPin=12;
 int dataPin=11;
-byte minute1, minute0, hour1, hour0;
+
+//global bytes
+byte time_raw[2]; //raw read from Real Time Clock
+byte time_formatted[4]; //high 4 bits contains digit to activate, low 4 bits contains number
+short unsigned int currentDigit=0; //current digit to display, it is incremented during the timer 1 ISR
 
 void setup() {
   pinMode(latchPin,OUTPUT);
@@ -26,6 +31,29 @@ void setup() {
   Wire.write(B00000100);
   Wire.endTransmission();
   
+  //do initial read of Real Time Clock and registers
+  Wire.beginTransmission(0x51);
+  Wire.write(0x03);
+  Wire.endTransmission();
+  //read timer registers
+  Wire.requestFrom(0x51,2);
+  int index = 0;
+  while(Wire.available()) {
+    time_raw[index]=Wire.read();
+    index++;
+  };
+  //format the time correctly by picking apart register contents
+  //first four bits determine which integer lights up. The final byte
+  //resembles lightSelection<<4 | numberInBinary
+  //Minute 0
+  time_formatted[0]=(B10000000) | (time_raw[0]&B00001111);
+  //Minute 1
+  time_formatted[1]=(B01000000) | ((time_raw[0]&B01110000)>>4);
+  //Hour 0
+  time_formatted[2]=(B00100000) | (time_raw[1]&B00001111);
+  //Hour 1
+  time_formatted[3]=(B00010000) | ((time_raw[1]&B00110000)>>4);
+  
   //set up timer and interrupts
   noInterrupts();
   TCCR1A=0;
@@ -33,16 +61,20 @@ void setup() {
   TCNT1=0;
   
   OCR1A=250; //16MHz/256 prescaler/250Hz (.004 s period);
-  TCCR1B |= (1<<WGM12); //CTC
+  TCCR1B |= (1<<WGM12); //set to CTC with OCR1A max
   TCCR1B |= (1<<CS12); //256 prescaler
   TIMSK1 |= (1<<OCIE1A); //enable timer compare interrupt
   interrupts();
 }
 
+ISR(TIMER1_COMPA_vect) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, time_formatted[(currentDigit++)%4]);
+  digitalWrite(latchPin,HIGH);
+};
+
 void loop() {
-  byte time_raw[2];
   //each byte contains a ready to send code
-  byte time_formatted[4];
   //set address to timer registers
   Wire.beginTransmission(0x51);
   Wire.write(0x03);
@@ -65,14 +97,13 @@ void loop() {
   time_formatted[2]=(B00100000) | (time_raw[1]&B00001111);
   //Hour 1
   time_formatted[3]=(B00010000) | ((time_raw[1]&B00110000)>>4);
-  //shift each formatted bit out to display, 4ms in between.
-  //later, this will be moved to timer-based itnerrupts.
-  for(int i=0;i<10;i++) {
-    for(int displaySelect=0;displaySelect<4;displaySelect++) {
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, time_formatted[displaySelect]);
-      digitalWrite(latchPin,HIGH);
-      delay(4);
-    };
-  };
+  //THIS HAS BEEN MOVED TO TIMER 1 CTC ISR
+//  for(int i=0;i<10;i++) {
+//    for(int displaySelect=0;displaySelect<4;displaySelect++) {
+//      digitalWrite(latchPin, LOW);
+//      shiftOut(dataPin, clockPin, MSBFIRST, time_formatted[displaySelect]);
+//      digitalWrite(latchPin,HIGH);
+//      delay(4);
+//    };
+//  };
 }
